1b1e6804014e878152b7c5d484589e9ddbc93c59
김승찬 <seungchan1219@gmail.com>
2019-04-12 19:55:04 +0900

    Add 소스.cpp

---
+++ 소스.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i < 9; i++)
+	{
+		cout << i << "단을 출력합니다." << endl;
+		for (int j = 1; j < 10; j++)
+			cout << i << " X " << j << " = " << i * j << endl;
+		cout << endl;
+	}
+	
+	return 0;
+}
